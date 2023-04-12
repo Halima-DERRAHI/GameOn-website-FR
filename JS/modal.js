165072d8da4e929067bb539f4c4571ed4ac0d50f
halima derrahi <halima.derrahi@gmail.com>
2023-04-12 11:50:32 +0200

    fields update

--- JS/modal.js
+++ JS/modal.js
@@ -17,15 +17,14 @@ const formData = document.querySelectorAll(".formData");
 const confirmMsg = document.querySelector(".message-confirm");
 
 // Form elements
-const firstName = document.getElementById ("first");
-const lastName = document.getElementById ("last");
-const email = document.getElementById ("email");
-const birthdate = document.getElementById ("birthdate");
-const quantity = document.getElementById ("quantity");
+const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
+const email = document.getElementById("email");
+const birthdate = document.getElementById("birthdate");
+const quantity = document.getElementById("quantity");
 const locationChoice = document.getElementById("location6");
 const CU = document.getElementById("checkbox1");
-const inputValid = document.querySelector(".input-valid");
-  
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -39,7 +38,7 @@ function launchModal() {
 }
 
 // close modal form
-function closeModal () {
+function closeModal() {
   modalbg.style.display = "none";
 }
 
@@ -52,21 +51,33 @@ function closeConfirmModal() {
 }
 
 // form submit
-form.addEventListener('submit', function(e) {
-
-    e.preventDefault();
-    validate();
-    if ((validateFirstName()) && (validateLastName()) && (validateEmail()) && (validateBirthDay()) && (validateQuantity()) &&( validationLocation()) && (validateCu())) {
-      confirmMessage();
-      hideAllMsg();
-      form.reset();
-    }
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  validate();
+  if (
+    validateFirstName() &&
+    validateLastName() &&
+    validateEmail() &&
+    validateBirthDay() &&
+    validateQuantity() &&
+    validationLocation() &&
+    validateCu()
+  ) {
+    confirmMessage();
+    hideAllMsg();
+    form.reset();
   }
-)
+});
+
+// Validation on event
+firstName.addEventListener("input", validateFirstName);
+lastName.addEventListener("input", validateLastName);
+email.addEventListener("input", validateEmail);
+birthdate.addEventListener("input", validateBirthDay);
+quantity.addEventListener("input", validateQuantity);
 
 // validation form
 function validate() {
-
   validateFirstName();
   validateLastName();
   validateEmail();
@@ -79,220 +90,220 @@ function validate() {
 
 // First name validation
 function validateFirstName() {
-
   // Check whitespaces
   if (!firstName.value.trim()) {
-    showError(firstName , "Veuillez renseigner un prénom." );
+    checkMsg(firstName, "Veuillez renseigner un prénom.", "input-error");
     return false;
   }
   // Check string length
-  else if (firstName.value.length <= 1) {
-    showError(firstName , "Veuillez entrer 2 caractères ou plus pour le champ du prenom." );
+  else if (firstName.value.length < 2) {
+    checkMsg(
+      firstName,
+      "Veuillez entrer 2 caractères ou plus pour le champ du prenom.",
+      "input-error"
+    );
     return false;
   }
   // Check if the string is valid
-  else if (!firstName.value.match(/^[a-zA-Z\s\-À-ÖØ-öø-ÿ']+$/g) ) {
-    showError(firstName , "Veuillez entrer un prénom valide.");
+  else if (!firstName.value.match(/^[a-zA-Z\s\-À-ÖØ-öø-ÿ']+$/g)) {
+    checkMsg(firstName, "Veuillez entrer un prénom valide.", "input-error");
     return false;
   }
   // Valid string
   else {
-  hideError (firstName);
-  return true;
-  } 
+    checkMsg(firstName, "", "input-valid");
+    return true;
+  }
 }
 
 // Last name validation
 function validateLastName() {
-
   // Check whitespaces
   if (!lastName.value.trim()) {
-    showError(lastName , "Veuillez renseigner un nom." );
+    checkMsg(lastName, "Veuillez renseigner un nom.", "input-error");
     return false;
   }
   // Check string length
-  else if (lastName.value.length <= 1) {
-    showError(lastName , "Veuillez entrer 2 caractères ou plus pour le champ du nom." );
+  else if (lastName.value.length < 2) {
+    checkMsg(
+      lastName,
+      "Veuillez entrer 2 caractères ou plus pour le champ du nom.",
+      "input-error"
+    );
     return false;
   }
   // Check if the string is valid
   else if (!lastName.value.match(/^[a-zA-Z\s\-À-ÖØ-öø-ÿ']+$/g)) {
-    showError(lastName , "Veuillez entrer un nom valide." );
+    checkMsg(lastName, "Veuillez entrer un nom valide.", "input-error");
     return false;
   }
   // Valid string
   else {
-    hideError (lastName);
+    checkMsg(lastName, "", "input-valid");
     return true;
-  } 
+  }
 }
 
 // email validation
 function validateEmail() {
-  
-  const emailRegex = /^[a-zA-Z][a-zA-Z0-9\-\_\.]+@[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}$/;
+  const emailRegex =
+    /^[a-zA-Z][a-zA-Z0-9\-\_\.]+@[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}$/;
   // Check whitespaces
   if (!email.value.trim()) {
-    showError(email , "Veuillez renseigner un E-mail.");
+    checkMsg(email, "Veuillez renseigner un E-mail.", "input-error");
     return false;
   }
   // Check if the string is valid
   else if (!email.value.match(emailRegex)) {
-    showError(email , "Veuillez renseigner un E-mail valide.");
+    checkMsg(email, "Veuillez renseigner un E-mail valide.", "input-error");
     return false;
   }
   // Valid string
   else {
-    hideError(email);
+    checkMsg(email, "", "input-valid");
     return true;
   }
 }
 
 // Limit the age at 12 years
 function limitAge(input) {
-
   const birthday = new Date(input.value);
   const today = new Date();
-  const age = today.getFullYear() - birthday.getFullYear(); 
+  const age = today.getFullYear() - birthday.getFullYear();
 
   if (age < 18 || age > 99) {
     return false;
-  }else {
+  } else {
     return true;
   }
 }
 
 // Birthday date validation
 function validateBirthDay() {
-
-  const dateRegex = /^[1-2][0-9][0-9][0-9]\-(0[1-9]|1[0-2])\-(0[1-9]|1\d|2\d|3[01])$/;
+  const dateRegex =
+    /^[1-2][0-9][0-9][0-9]\-(0[1-9]|1[0-2])\-(0[1-9]|1\d|2\d|3[01])$/;
   // Check if the value is valid
   if (!dateRegex.test(birthdate.value)) {
-    showError(birthdate , "Veuillez renseigner une date de naissance valide.");
+    checkMsg(
+      birthdate,
+      "Veuillez renseigner une date de naissance valide.",
+      "input-error"
+    );
     return false;
   }
   // Check the age group
-  else if (!limitAge(birthdate)){
-    showError(birthdate , "Vous devez avoir entre 18 et 99 ans pour pouvoir faire une réservation.");
+  else if (!limitAge(birthdate)) {
+    checkMsg(
+      birthdate,
+      "Vous devez avoir entre 18 et 99 ans pour pouvoir faire une réservation.",
+      "input-error"
+    );
     return false;
   }
   // Valid value
   else {
-    hideError(birthdate);
+    checkMsg(birthdate, "", "input-valid");
     return true;
   }
 }
 
 // Quantity validation
 function validateQuantity() {
-
   const quantityRegex = /^[0-9]{1,2}$/;
 
   if (!quantity.value) {
-    showError(quantity , "Veuillez renseigner a combien de tournois GameOn avez-vous déjà participé.");
+    checkMsg(
+      quantity,
+      "Veuillez renseigner a combien de tournois GameOn avez-vous déjà participé.",
+      "input-error"
+    );
     return false;
-  } 
+  }
   // Check string length
-  else if (quantity.value.length > 2){
-    showError(quantity , "Veuillez renseigner un nombre à deux chiffres maximum.");
+  else if (quantity.value.length > 2) {
+    checkMsg(
+      quantity,
+      "Veuillez renseigner un nombre à deux chiffres maximum.",
+      "input-error"
+    );
     return false;
   }
   // Check if the value is valid
   else if (!quantityRegex.test(quantity.value)) {
-    showError(quantity , "Veuillez renseigner un nombre valide.");
+    checkMsg(quantity, "Veuillez renseigner un nombre valide.", "input-error");
     return false;
   }
   // Valid value
   else {
-    hideError(quantity);
+    checkMsg(quantity, "", "input-valid");
     return true;
   }
 }
 
 // Location validation
-function validationLocation(){
-
+function validationLocation() {
   const radioCheck = document.querySelector('input[name = "location"]:checked');
 
-  if (!radioCheck){
-    checkboxError(locationChoice , "Veuillez renseigner une localisation.");
+  if (!radioCheck) {
+    checkboxMsg(locationChoice, "Veuillez renseigner une localisation.");
     return false;
   }
   // Valid value
   else {
-    hidecheckboxError(locationChoice);
+    checkboxMsg(locationChoice, " ");
     return true;
   }
 }
 
 // CU validations
 function validateCu() {
-  
   if (CU.checked) {
-    hidecheckboxError(CU);
+    checkboxMsg(CU, " ");
     return true;
   }
   // Not checked
   else {
-    checkboxError(CU , "Vous devez vérifier que vous acceptez les termes et conditions d'utilisation.");
+    checkboxMsg(
+      CU,
+      "Vous devez vérifier que vous acceptez les termes et conditions d'utilisation."
+    );
     return false;
   }
 }
 
-// error message function
-function showError(input , message) {
-
+// Verification message
+function checkMsg(input, message, classChoice) {
   const formDataInput = input.parentElement;
-  const msgError = formDataInput.querySelector("small");
+  const fieldMsg = formDataInput.querySelector("small");
 
-  msgError.innerText = message;
-  input.className = "text-control input-error";
+  fieldMsg.innerText = message;
+  input.className = "text-control " + classChoice;
 }
 
-// error checkbox function
-function checkboxError(input , message) {
-
+// Verification checkbox
+function checkboxMsg(input, message) {
   const formDataInput = input.parentElement;
-  const msgError = formDataInput.querySelector("small");
+  const fieldMsg = formDataInput.querySelector("small");
 
-  msgError.innerText = message;
-}
-
-// hide error message function
-function hideError(input) {
-
-  const formDataInput = input.parentElement;
-  const msgError = formDataInput.querySelector("small");
-
-  msgError.innerText = " ";
-  input.className = "text-control";
-}
-
-// hide error checkbox function
-function hidecheckboxError(input) {
-
-  const formDataInput = input.parentElement;
-  const msgError = formDataInput.querySelector("small");
-  
-  msgError.innerText = " ";
+  fieldMsg.innerText = message;
 }
 
 // cofirm message
 function confirmMessage() {
-
-  form.style.display = "none"; 
-  confirmMsg.innerHTML = "<p>Merci !<br> Votre réservation a été reçue.</p>" + '<button class="btn-submit button" onclick="closeConfirmModal()">Fermer</button>';
+  form.style.display = "none";
+  confirmMsg.innerHTML =
+    "<p>Merci pour <br>votre inscription.</p>" +
+    '<button class="btn-submit button" onclick="closeConfirmModal()">Fermer</button>';
   confirmMsg.style.display = "block";
 }
 
-// Reset all messages
-function hideAllMsg () {
-
-  hideError(firstName); 
-  hideError(lastName); 
-  hideError(email); 
-  hideError(birthdate); 
-  hideError(quantity); 
-  hidecheckboxError(locationChoice); 
-  hidecheckboxError(CU);
+// Remove all messages
+function hideAllMsg() {
+  checkMsg(firstName, "", "");
+  checkMsg(lastName, "", "");
+  checkMsg(email, "", "");
+  checkMsg(birthdate, "", "");
+  checkMsg(quantity, "", "");
+  checkboxMsg(locationChoice, "");
+  checkboxMsg(CU, "");
 }
